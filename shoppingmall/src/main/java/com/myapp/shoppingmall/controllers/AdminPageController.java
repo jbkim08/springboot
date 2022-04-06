92ed246c72914fc1abbfc5b71c292b61ed55d1e8
kjb <drv98@naver.com>
2022-04-06 10:37:12 +0900

    쇼핑몰 수정 페이지

--- shoppingmall/src/main/java/com/myapp/shoppingmall/controllers/AdminPageController.java
+++ shoppingmall/src/main/java/com/myapp/shoppingmall/controllers/AdminPageController.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myapp.shoppingmall.dao.PageRepository;
 import com.myapp.shoppingmall.entities.Page;
@@ -23,34 +24,78 @@ public class AdminPageController {
 
 	@Autowired
 	private PageRepository pageRepo;
-	
+
 	@GetMapping
 	public String index(Model model) {
 		List<Page> pages = pageRepo.findAll();
 		model.addAttribute("pages", pages);
 		return "admin/pages/index";
 	}
-	
-	@GetMapping("/add")
+
+	@GetMapping("/add") // /admin/pages/add
 	public String add(@ModelAttribute Page page) {
-		//model.addAttribute("page", new Page());
+		// model.addAttribute("page", new Page());
 		return "admin/pages/add";
 	}
-	
+
 	@PostMapping("/add")
-	public String add(@Valid Page page, BindingResult bindingResult) {
-		//유효성검사 결과 에러가 있으면 다시 돌아감
-		if(bindingResult.hasErrors()) return "admin/pages/add";
-		
-		return "redirect:admin/pages/add";
+	public String add(@Valid Page page, BindingResult bindingResult, RedirectAttributes attr) {
+		// 유효성검사 결과 에러가 있으면 다시 돌아감
+		if (bindingResult.hasErrors())
+			return "admin/pages/add";
+		// 검사 통과시
+		attr.addFlashAttribute("message", "성공적으로 페이지 추가됨");
+		attr.addFlashAttribute("alertClass", "alert-success"); // 부트스트랩 경고창(성공색)
+
+		// 슬러그 검사(슬러그 미 입력시 제목을 소문자로하고 공백은 -으로 대체), 입력시에도 소문자 공백은 -대체
+		String slug = (page.getSlug() == "") ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug();
+		Page slugExist = pageRepo.findBySlug(slug); // 슬러그로 DB검색하여 있으면 Page로 리턴
+
+		if (slugExist != null) { // 같은 슬러그가 있을시 저장 안됨
+			attr.addFlashAttribute("message", "슬러그가 이미 있습니다. 다른것을 고르세요");
+			attr.addFlashAttribute("alertClass", "alert-danger"); // 부트스트랩 경고창(경고색)
+			attr.addFlashAttribute("page", page); // 부트스트랩 경고창(경고색)
+		} else {
+			page.setSlug(slug); // 소문자 - 수정된 슬러그를 업데이트
+			page.setSorting(100); // 기본 솔팅값
+
+			pageRepo.save(page);
+		}
+
+		return "redirect:/admin/pages/add"; // post-redirect-get
 	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id, Model model) {
+		Page page = pageRepo.getById(id); // 테이블에서 id로 page 검색
+		model.addAttribute("page", page); // 수정페이지에 page 정보객체를 전달
+		return "admin/pages/edit"; // 수정 페이지
+	}
+
+	@PostMapping("/edit")
+	public String edit(@Valid Page page, BindingResult bindingResult, RedirectAttributes attr) {
+		//유효성검사 결과 에러가 있으면 다시 돌아감
+		if (bindingResult.hasErrors()) return "admin/pages/edit";
+		//검사 통과시 
+		attr.addFlashAttribute("message", "성공적으로 페이지 수정됨");
+		attr.addFlashAttribute("alertClass", "alert-success"); // 부트스트랩 경고창(성공색)
+
+		//슬러그 검사(슬러그 미 입력시 제목을 소문자로하고 공백은 -으로 대체), 입력시에도 소문자 공백은 -대체
+		String slug = (page.getSlug() == "") ? page.getTitle().toLowerCase().replace(" ", "-") : page.getSlug();
+		Page slugExist = pageRepo.findBySlugAndIdNot(slug, page.getId()); // 슬러그로 DB검색하여 있으면 Page로 리턴(현재 id의 슬러그는 제외)
+
+		if (slugExist != null) { // 같은 슬러그가 있을시 저장 안됨
+			attr.addFlashAttribute("message", "슬러그가 이미 있습니다. 다른것을 고르세요");
+			attr.addFlashAttribute("alertClass", "alert-danger"); // 부트스트랩 경고창(경고색)
+			attr.addFlashAttribute("page", page); 
+		} else {
+			page.setSlug(slug); // 소문자 - 수정된 슬러그를 업데이트
+			page.setSorting(100); // 기본 솔팅값
+
+			pageRepo.save(page);
+		}
+
+		return "redirect:/admin/pages/edit/" + page.getId(); // post-redirect-get
+	}
+
 }
-
-
-
-
-
-
-
-
-
--- shoppingmall/src/main/java/com/myapp/shoppingmall/dao/PageRepository.java
+++ shoppingmall/src/main/java/com/myapp/shoppingmall/dao/PageRepository.java
@@ -6,5 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.myapp.shoppingmall.entities.Page;
 
 public interface PageRepository extends JpaRepository<Page, Integer> {
-	// 리스트<Page>로 리턴되는 findAll 등 여러 메소드 추가됨, 페이징 sorting
+	// 리스트<Page>로 리턴되는 findAll 등 여러 메소드 추가됨, 페이징 sorting	
+	Page findBySlug(String slug); // 실제 구현은 jpa 하이버네이트가 함
+
+	Page findBySlugAndIdNot(String slug, int id); // 슬러그를 찾는데 현재 id로 찾은것은 제외
 }
